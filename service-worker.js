0b4ca14aa9b961b0718c5562e89a4f4addb27ba9
peediouf90 <77977018+peediouf90@users.noreply.github.com>
2026-07-03 19:28:51 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
  * forcer la mise à jour du cache chez les utilisateurs.
  */
 
-const CACHE_VERSION = 'gescom-v3';
+const CACHE_VERSION = 'gescom-v4';
 const CACHE_APP_SHELL = `${CACHE_VERSION}-app-shell`;
 
 // Liste exhaustive des fichiers nécessaires au fonctionnement hors-ligne complet
